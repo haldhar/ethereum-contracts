4d598f869c9b85c8791e8234f26c814000c2c2d6
Bill Marino <billmarino@gmail.com>
2016-01-07 02:35:01 -0500

    Add Rental Agreement .sol

---
+++ rentalagreement.sol
@@ -0,0 +1,59 @@
+contract RentalAgreement {
+    address apartmentOwner;
+    address apartmentRenter;
+    string  decryptionPassword;
+    bool rentPaid = false;
+    bool keySent = false;
+    bool keyReceived = false;
+    uint rentalDate;
+    uint rentalFee;
+    
+    
+    function RentalAgreement(address renter, uint _rentalFee, uint _rentalDate){
+        apartmentOwner = msg.sender; 
+        apartmentRenter = renter; 
+        rentalFee = _rentalFee;   
+        rentalDate = _rentalDate; 
+    }
+    
+    function payRent(){
+        if (msg.sender == apartmentRenter && now <= rentalDate && msg.value >= rentalFee) {
+                rentPaid = true;
+            } else {
+                msg.sender.send(msg.value);
+            }
+    }
+    
+    function setKey(string _decryptionPassword){
+               if (msg.sender == apartmentOwner && now <= rentalDate && rentPaid == true){
+                   decryptionPassword = _decryptionPassword;
+                   keySent = true;
+                   msg.sender.send(this.balance);
+               }
+        
+    }
+    
+        function getKey() public returns (string) {
+               if (msg.sender == apartmentRenter && now > rentalDate && rentPaid == true && keySent == true){
+                   keyReceived = true;
+                   return decryptionPassword;
+               }
+        
+    }
+    
+        function refund(){
+               if (msg.sender == apartmentRenter && now > rentalDate && rentPaid == true && keySent == false){
+                   msg.sender.send(this.balance);
+               }
+        
+    }
+    
+            function kill(){
+               if (msg.sender == apartmentOwner && now > rentalDate && rentPaid == false && keySent == true && keyReceived == true){
+                   suicide(apartmentOwner);
+               }
+        
+    }
+    
+    
+}
